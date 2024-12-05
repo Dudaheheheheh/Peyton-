9ac53e4102873845145f784555fc7e4c3b39b831
Dudaheheheheh <madufernandes013@gmail.com>
2024-12-05 11:24:45 -0300

    Add files via upload

---
+++ Peyton aula - 2/aula02.py
@@ -0,0 +1,9 @@
+print ('Hi, my name is Maria Eduarda')
+nome_usuario = 'Maria Eduarda' # tipo -> str = string
+idade_usuario = 18 # tipo -> int = integer
+altura_usuario = 1.60 # tipo -> float = flutuante
+maior_idade = True # tipo -> bool = booleasn
+print ('Seja bem vindo', nome_usuario) 
+print(' Sua idade é', idade_usuario, 'anos')
+print('Você tem', altura_usuario, 'metros.')
+print('Você é maior de idade?', maior_idade)
